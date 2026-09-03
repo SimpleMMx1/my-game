ed23929ddec443b2873551582efc583bb1fd295d
Maitri <maitripratapshakya@gmail.com>
2026-09-03 08:50:33 +0545

    asyncawait

---
+++ asyncawait.js
@@ -0,0 +1,26 @@
+const checkLogin=()=>{
+    
+return new Promise
+((resolve,reject)=>{
+let loggedIn=true;
+if(loggedIn){
+    resolve("welcome you are logged in")
+}
+else{
+    reject("sorry you are not logged in")
+}
+}
+)
+}
+
+async function handleLogin(){
+    try{
+        const result=await 
+        checkLogin();
+        console.log(result);
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+    handleLogin();
